2c342707194fe4883b9efaa83a5379bf762a8ef3
Richard Ayikwei <168413251+richardayikwei@users.noreply.github.com>
2024-11-18 04:11:08 +0000

    Initials art

---
+++ 1-hello-world/block-letters/initialsArt.py
@@ -0,0 +1,10 @@
+# Richard Ayikwei
+# Change is constant
+
+print('R R R R          A')
+print('R       R      A   A')
+print('R       R    A       A')
+print('R R R R      A A A A A')
+print('R   R        A       A')
+print('R     R      A       A')
+print('R       R    A       A')
